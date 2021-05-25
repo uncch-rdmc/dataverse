3a2def692b513f74954746855e567c6b80e0a83c
don.sizemore <don.sizemore@github.com>
2021-05-25 14:50:15 -0400

    call integration test list from tests/integration-tests.txt

--- conf/docker-aio/run-test-suite.sh
+++ conf/docker-aio/run-test-suite.sh
@@ -6,6 +6,8 @@ if [ -z "$dvurl" ]; then
 	dvurl="http://localhost:8084"
 fi
 
+integrationtests=$(<tests/integration-tests.txt)
+
 # Please note the "dataverse.test.baseurl" is set to run for "all-in-one" Docker environment.
 # TODO: Rather than hard-coding the list of "IT" classes here, add a profile to pom.xml.
-source maven/maven.sh && mvn verify -P all-integration-tests -DskipUnitTests -Ddataverse.test.baseurl=$dvurl
+source maven/maven.sh && mvn test -Dtest=$integrationtests -Ddataverse.test.baseurl=$dvurl
